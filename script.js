f96a2a3b9ecae07765e47487927fe911ffa32521
Mertaag <141136569+Mertaag@users.noreply.github.com>
2024-08-06 12:15:13 -0400

    Set up js skeleton, to do functions

--- script.js
+++ script.js
@@ -7,29 +7,63 @@ function subtract(num1, num2) {
 };
 
 function multiply(num1, num2) {
- return num1 * num2;
+    return num1 * num2;
 };
 
 function divide(num1, num2) {
     return num1 / num2;
 };
 
-let num1;
-let num2;
-let operator;
-
 function operate(operator, num1, num2) {
     switch (operator) {
-        case '+' : return add(num1, num2);
-        break;
-        case '-' : return subtract(num1, num2);
-        break;
-        case '*' : return multiply(num1, num2);
-        break;
-        case '/' : return divide(num1, num2);
-        break;
-        
+        case '+': return add(num1, num2);
+            break;
+        case '-': return subtract(num1, num2);
+            break;
+        case '*': return multiply(num1, num2);
+            break;
+        case '/': if(num2 === 0) return null
+            else return divide(num1, num2);
+            break;
+
         default:
-        console.log('Choose the correct operation');
+            console.log('Choose the correct operation');
     }
+}
+
+
+const numbersBtn = document.querySelectorAll('.num-button');
+const operationsBtn = document.querySelectorAll('.ops.button');
+const equalsBtn = document.getElementById('equals');
+const clearBtn = document.getElementById('clear');
+const deleteBtn = document.getElementById('delete');
+
+let lastValue = document.getElementById('last-value');
+let CurrentValue = document.getElementById('current-value');
+
+equalsBtn.addEventListener ('click', evaluate);
+clearBtn.addEventListener ('click', clear);
+deleteBtn.addEventListener ('click', deleteInput);
+
+numbersBtn.forEach((button) => 
+    button.addEventListener('click', () => CurrentValue.innerText += button.value));
+
+operationsBtn.forEach((button) =>
+    button.addEventListener('click', () => setOperation(button.value)));
+
+
+function evaluate() {
+
+}
+
+function clear() {
+
+}
+
+function deleteInput() {
+
+}
+
+function setOperation(operator) {
+
 }
