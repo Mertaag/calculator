d666a7f8b2e51557a55a7c9e0773af940d01d7aa
Mertaag <141136569+Mertaag@users.noreply.github.com>
2024-08-07 14:33:51 -0400

    Work through operation functions, needs fix

--- script.js
+++ script.js
@@ -22,8 +22,8 @@ function operate(operator, num1, num2) {
             break;
         case '*': return multiply(num1, num2);
             break;
-        case '/': if(num2 === 0) return null
-            else return divide(num1, num2);
+        case '/': if (num2 === 0) return null
+        else return divide(num1, num2);
             break;
 
         default:
@@ -33,37 +33,60 @@ function operate(operator, num1, num2) {
 
 
 const numbersBtn = document.querySelectorAll('.num-button');
-const operationsBtn = document.querySelectorAll('.ops.button');
+const operationsBtn = document.querySelectorAll('.ops-button');
 const equalsBtn = document.getElementById('equals');
 const clearBtn = document.getElementById('clear');
 const deleteBtn = document.getElementById('delete');
 
 let lastValue = document.getElementById('last-value');
-let CurrentValue = document.getElementById('current-value');
+let currentValue = document.getElementById('current-value');
+let currentOperation = '';
+let operand1 = '';
+let operand2 = '';
 
-equalsBtn.addEventListener ('click', evaluate);
-clearBtn.addEventListener ('click', clear);
-deleteBtn.addEventListener ('click', deleteInput);
+equalsBtn.addEventListener('click', evaluate);
+clearBtn.addEventListener('click', clear);
+deleteBtn.addEventListener('click', deleteInput);
 
-numbersBtn.forEach((button) => 
-    button.addEventListener('click', () => CurrentValue.innerText += button.value));
+numbersBtn.forEach((button) =>
+    button.addEventListener('click', () => {
+        if (currentValue.innerText === '0') currentValue.innerText ='';
+        currentValue.innerText += button.value
+    }));
 
 operationsBtn.forEach((button) =>
     button.addEventListener('click', () => setOperation(button.value)));
 
+function setOperation(operator) {
+    if (currentOperation !== '') evaluate()
+    else {
+        operand1 = currentValue.textContent;
+        currentOperation = operator;
+        lastValue.textContent = `${operand1} ${currentOperation}`;
+    }
+}
 
 function evaluate() {
-
+    if (currentValue.innerText === '0' && currentOperation === '/') {
+        alert('No division by 0 allowed, please try something else');
+    }
+    else {
+        operand2 = currentValue.textContent;
+        currentValue.textContent = operate(currentOperation, operand1, operand2);
+        lastValue.textContent = `${operand1} ${currentOperation} ${operand2} =`;
+        currentOperation = '';
+    }
 }
 
 function clear() {
-
+    currentValue.textContent = '0';
+    lastValue.textContent = '';
+    operand1 = '';
+    operand2 = '';
+    currentOperation = '';
 }
 
 function deleteInput() {
 
 }
 
-function setOperation(operator) {
-
-}
